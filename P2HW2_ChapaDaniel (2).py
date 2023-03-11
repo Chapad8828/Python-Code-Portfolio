b8d9d2f5b6aa93ef56c198bdbdf1a2059b10e63c
Chapad8828 <125828664+Chapad8828@users.noreply.github.com>
2023-03-11 11:28:59 -0500

    Add files via upload
    #CTI-110
    #P2HW2-List-Output
    #Daniel_Chapa
    #02/26/2023

---
+++ P2HW2_ChapaDaniel (2).py
@@ -0,0 +1,18 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: C:\Users\dceag\Desktop\P2HW2_Modules_List_ChapaDaniel.py ======
+The number of grades for all six modules:
+36
+The lowest grade for all six modules:
+50
+The highest grade for all six modules:
+100
+The total of the grades for all six modules:
+2725
+The average grade for all six modules:
+75.69444444444444
+
+
+All grades
+(50, 60, 70, 80, 90, 100, 55, 65, 75, 85, 95, 100, 60, 65, 70, 75, 80, 85, 80, 80, 85, 85, 90, 95, 55, 65, 75, 85, 95, 100, 50, 55, 60, 65, 70, 75)
